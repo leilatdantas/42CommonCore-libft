0036b19037baafd6217663632f84cdbdb3dc9549
Leila Dantas <leiladantas@macbook-air.lan>
2023-04-25 20:02:36 +0100

    tuesday updates

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: lebarbos <lebarbos@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 19:17:24 by lebarbos          #+#    #+#             */
-/*   Updated: 2023/04/25 19:48:49 by lebarbos         ###   ########.fr       */
+/*   Updated: 2023/04/25 19:59:26 by lebarbos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,21 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int	mult;
+	unsigned int	nbr;
 
-	mult = 10;
-	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
-	else if (n == 2147483647)
-		ft_putstr_fd("2147483647", fd);
-	else
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			ft_putchar_fd('-', fd);
-			n = -n;
-		}
-		while (mult <= n)
-			mult = mult * 10;
-		mult = mult / 10;
-		while (mult >= 10)
-		{
-			ft_putchar_fd((((n / mult) % 10) + '0'), fd);
-			mult = mult / 10;
-		}
-		ft_putchar_fd((n % 10 + '0'), fd);
+		ft_putchar_fd('-', fd);
+		nbr = -n;
 	}
+	else
+		nbr = n;
+	if (nbr >= 10)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd((nbr % 10 + '0'), fd);
 }
 
 /*int main (void)
 {
-	ft_putnbr_fd(-214748364, 1);
+	ft_putnbr_fd(2147483647, 1);
 }*/
