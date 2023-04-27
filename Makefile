fddaad651e99bc2165f9c9d4f1e47e4528308f60
Leila Ticiane Barbosa De Lima Dantas <lebarbos@c1r15s7.42porto.com>
2023-04-27 15:02:14 +0100

    Makefile updates

--- Makefile
+++ Makefile
@@ -3,37 +3,52 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: lebarbos <lebarbos@student.42porto.co      +#+  +:+       +#+         #
+#    By: lebarbos <lebarbos@student.42porto.com     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/04/20 15:26:15 by lebarbos          #+#    #+#              #
-#    Updated: 2023/04/20 15:27:52 by lebarbos         ###   ########.fr        #
+#    Created: 2023/04/27 13:31:42 by lebarbos          #+#    #+#              #
+#    Updated: 2023/04/27 14:59:03 by lebarbos         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME = libft.a
+CC		= gcc
 
-SRCS = $(wildcard *.c)
+CFLAGS	= -Wall -Wextra -Werror
 
-OBJS = $(SRCS:.c=.o)
+LIB1	= ar -rcs
 
-CC = cc
-CFLAGS = -Wall -Werror -Wextra
+LIB2	= ranlib
 
-all: $(NAME)
+RM		= /bin/rm -f
 
-$(NAME): $(OBJS)
-	ar rc $(NAME) $(OBJS)
-	ranlib $(NAME)
+NAME	= libft.a
 
-%.o: %.c
-	$(CC) $(CFLAGS) -c $< -o $@
+INCLUDE	= libft.h
+SRCS	= ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
+			ft_toupper.c ft_tolower.c ft_strncmp.c ft_memcmp.c ft_atoi.c \
+			ft_strlen.c ft_strlcpy.c ft_strlcat.c ft_memset.c ft_bzero.c \
+			ft_memcpy.c ft_memmove.c ft_memchr.c ft_strchr.c ft_strrchr.c \
+			ft_strnstr.c ft_strdup.c ft_calloc.c ft_substr.c ft_strjoin.c \
+			ft_strtrim.c ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c \
+			ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c ft_putnbr_fd.c
+
+OBJS	= $(SRCS:.c=.o)
+
+all:		$(NAME)
+
+$(NAME):	$(OBJS) $(INCLUDE)
+			$(LIB1) $(NAME) $(OBJS)
+			$(LIB2) $(NAME)
+
+c.o.:
+			$(CC) $(FLAGS) -I$(INCLUDE) -c $< -o $(<:.c=.o)
 
 clean:
-	rm -f $(OBJS)
+			$(RM) $(OBJS)
 
-fclean: clean
-	rm -f $(NAME)
+fclean:		clean
+			$(RM) $(NAME)
 
-re: fclean all
+re:			fclean all
 
-.PHONY: all clean fclean re
+.PHONY:		all clean fclean re
+		
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lebarbos <lebarbos@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: lebarbos <lebarbos@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 12:46:53 by lebarbos          #+#    #+#             */
-/*   Updated: 2023/04/25 19:32:15 by lebarbos         ###   ########.fr       */
+/*   Updated: 2023/04/27 14:03:49 by lebarbos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 # include <stddef.h>
 # include <stdlib.h>
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
+/*
+**  Parte 1 - Functions from libc.
+*/
+
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -27,29 +28,36 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-int		ft_atoi(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_atoi(const char *str);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
-void	*ft_calloc(size_t nmemb, size_t size);
-void	ft_bzero(void *s, size_t n);
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
+void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strdup(const char *s);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+/*
+**  Parte 2 - Additional functions
+*/
 char	*ft_substr(char const *s, unsigned int star, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
