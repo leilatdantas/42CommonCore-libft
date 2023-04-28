eb95785bf21277bb565d067ea3710301addbdd6a
Leila Ticiane Barbosa De Lima Dantas <lebarbos@c1r16s7.42porto.com>
2023-04-28 11:39:02 +0100

    updates

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lebarbos <lebarbos@student.42porto.com     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 12:46:53 by lebarbos          #+#    #+#             */
-/*   Updated: 2023/04/28 10:59:53 by lebarbos         ###   ########.fr       */
+/*   Updated: 2023/04/28 11:12:53 by lebarbos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,6 +73,7 @@ t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
 int		ft_lstsize(t_list *lst);
 
 #endif
